4a3ba48054ffdc68e0ccc6158babea69abbb5361
YESOL KIM <kysloves@naver.com>
2023-04-10 15:52:57 +0900

    :wrench: make webpack to use babel when handling js/jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,22 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
   },
+  module: {
+    // the way how to webpack handles files
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  // how to resolve extensions in order. if the files have same name, webpack will resolve the file in order below
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
   // webpack-dev-server module picks up these options
   devServer: {
     // static files gonna be served from here. Default is 'public' directory
