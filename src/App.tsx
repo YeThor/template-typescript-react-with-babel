dca2c4f660f121ec0326b55470033bf025f0f15a
YESOL KIM <kysloves@naver.com>
2023-04-17 00:13:23 +0900

    :wrench: do type-check before building/serving code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-const App = () => <h1>Hello world</h1>;
+const App = () => {
+  return <h1>Hello world</h1>;
+};
+
+// the commented function below will emit type-error (noImplicitAny) when you run commands "npm run tscheck"
+// function fn(s) {
+//   console.log(s);
+// }
 
 export default App;
